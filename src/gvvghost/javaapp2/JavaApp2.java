bb6c25a15e71a30c4eab25ac8824963c9ada2f0e
GVVGhost <gvv.ghost@gmail.com>
2017-11-25 16:36:15 +0200

    Внесено зміни в файл JavaApp2.java

--- src/gvvghost/javaapp2/JavaApp2.java
+++ src/gvvghost/javaapp2/JavaApp2.java
@@ -1,4 +1,28 @@
 package gvvghost.javaapp2;
+import java.net.*;
+import java.util.*;
+import java.io.*;
 
-public class JavaApp2 {
+public class JavaApp2
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Socket clientSocket = new Socket("192.168.0.100" , 5555);
+            InputStream inputStream = clientSocket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            String messege = dataInputStream.readUTF();
+            System.out.println(messege);
+            dataInputStream.close();
+            inputStream.close();
+            clientSocket.close();
+        }
+        catch (ConnectException e)
+        {
+            System.out.println("Cannot connect!");
+        }
+        catch (IOException e)
+        {}
+    }
 }
